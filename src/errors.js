b210c344bd1c389592f28854ea359d23d3d91a4c
Stephen Caldwell <steve@campusexplorer.com>
2018-10-02 13:37:58 -0700

    refactor(errors): change how we structure error module

--- src/errors.js
+++ src/errors.js
@@ -1,4 +1,26 @@
-module.exports.EventPayloadError = class EventPayloadError extends Error {
+class AggregateNotFoundError extends Error {
+    constructor(aggregateName, aggregateId) {
+        super(
+            `cannot find aggregate "${aggregateName}" with id "${aggregateId}"`,
+        );
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+        this.aggregateName = aggregateName;
+        this.aggregateId = aggregateId;
+    }
+}
+
+class DuplicateAggregateError extends Error {
+    constructor(aggregateName, aggregateId) {
+        super(`"${aggregateName}" with id "${aggregateId}" already exists`);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+        this.aggregateName = aggregateName;
+        this.aggregateId = aggregateId;
+    }
+}
+
+class EventPayloadError extends Error {
     constructor(event, key) {
         const message = `event payload missing "${key}"`;
         super(message);
@@ -7,19 +29,65 @@ module.exports.EventPayloadError = class EventPayloadError extends Error {
         this.event = event;
         this.key = key;
     }
-};
+}
 
-module.exports.InvalidEventError = class InvalidEventError extends Error {
+class InvalidAggregateError extends Error {
+    constructor(message, aggregateName) {
+        super(`invalid aggregate "${aggregateName}": ${message}`);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+        this.aggregateName = aggregateName;
+    }
+}
+
+class InvalidEventError extends Error {
     constructor(message, event) {
         super(message);
         this.name = this.constructor.name;
         Error.captureStackTrace(this, this.constructor);
         this.event = event;
     }
-};
+}
 
-// eslint-disable-next-line max-len
-module.exports.InvariantViolatedError = class InvariantViolatedError extends Error {
+class InvalidCommandError extends Error {
+    constructor(message, aggregateName, commandName) {
+        super(
+            `aggregate "${aggregateName}": command "${commandName}" ` +
+                `is invalid: ${message}`,
+        );
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+        this.aggregateName = aggregateName;
+        this.commandName = commandName;
+    }
+}
+
+class InvalidCommandParamsError extends Error {
+    constructor(message, aggregateName, commandName) {
+        super(
+            `aggregate "${aggregateName}": command "${commandName}" ` +
+                `called with invalid params: ${message}`,
+        );
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+        this.aggregateName = aggregateName;
+        this.commandName = commandName;
+    }
+}
+
+class InvalidProjectionError extends Error {
+    constructor(message, aggregateName) {
+        super(
+            `aggregate "${aggregateName}" has an invalid projection: ` +
+                `${message}`,
+        );
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+        this.aggregateName = aggregateName;
+    }
+}
+
+class InvariantViolatedError extends Error {
     constructor(aggregateName, aggregateId, state, message) {
         super(message);
         this.name = this.constructor.name;
@@ -28,9 +96,23 @@ module.exports.InvariantViolatedError = class InvariantViolatedError extends Err
         this.aggregateId = aggregateId;
         this.state = state;
     }
-};
+}
 
-module.exports.UnauthorizedError = class UnauthorizedError extends Error {
+class MaxCommandAttemptsError extends Error {
+    constructor(aggregateName, commandName, attempts) {
+        super(
+            `aggregate "${aggregateName}": command "${commandName}" failed ` +
+                `after ${attempts} attempts`,
+        );
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+        this.aggregateName = aggregateName;
+        this.commandName = commandName;
+        this.attempts = attempts;
+    }
+}
+
+class UnauthorizedError extends Error {
     constructor(operation, userDesc) {
         const message = `user ${userDesc} is not allowed to call ${
             operation.type
@@ -41,10 +123,9 @@ module.exports.UnauthorizedError = class UnauthorizedError extends Error {
         this.operation = operation;
         this.userDesc = userDesc;
     }
-};
+}
 
-// eslint-disable-next-line max-len
-module.exports.UnknownAggregateError = class UnknownAggregateError extends Error {
+class UnknownAggregateError extends Error {
     constructor(aggregateName) {
         const message = `unknown aggregate "${aggregateName}"`;
         super(message);
@@ -52,10 +133,22 @@ module.exports.UnknownAggregateError = class UnknownAggregateError extends Error
         Error.captureStackTrace(this, this.constructor);
         this.aggregateName = aggregateName;
     }
-};
+}
 
-// eslint-disable-next-line max-len
-module.exports.UnknownEventTypeError = class UnknownEventTypeError extends Error {
+class UnknownCommandError extends Error {
+    constructor(aggregateName, commandName) {
+        const message =
+            `aggregate "${aggregateName}": unknown command ` +
+            `"${commandName}"`;
+        super(message);
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
+        this.aggregateName = aggregateName;
+        this.commandName = commandName;
+    }
+}
+
+class UnknownEventTypeError extends Error {
     constructor(eventType) {
         const message = `unknown event type "${eventType}"`;
         super(message);
@@ -63,4 +156,21 @@ module.exports.UnknownEventTypeError = class UnknownEventTypeError extends Error
         Error.captureStackTrace(this, this.constructor);
         this.eventType = eventType;
     }
+}
+
+module.exports = {
+    AggregateNotFoundError,
+    DuplicateAggregateError,
+    EventPayloadError,
+    InvalidAggregateError,
+    InvalidCommandError,
+    InvalidCommandParamsError,
+    InvalidEventError,
+    InvalidProjectionError,
+    InvariantViolatedError,
+    MaxCommandAttemptsError,
+    UnauthorizedError,
+    UnknownAggregateError,
+    UnknownCommandError,
+    UnknownEventTypeError,
 };
